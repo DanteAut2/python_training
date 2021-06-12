fc0b8b5515d7a7268a95c6643dd777faa852e0a3
Пархимчик Ярослав <yaroslav.parhim2@gmail.com>
2021-06-12 03:07:12 +0300

    Выполнил задание 3

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+
+    def __init__(self, first_name, home_number, first_mail, second_address):
+        self.first_name = first_name
+        self.home_number = home_number
+        self.first_mail = first_mail
+        self.second_address = second_address
---
+++ test_add_contact.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from contact import Contact
+import unittest, time, re
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+    
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_main_page(self, driver):
+        driver.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_number)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.first_mail)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.second_address)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def login(self, wd, login, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def tearDown(self):
+        self.wd.quit()
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.create_contact(wd, Contact(first_name="sdfdsfg", home_number="fdsfgsdfg", first_mail="sdfgsdffg",
+                                         second_address="fgdsfgsdfg"))
+        self.return_to_main_page(wd)
+        self.logout(wd)
+
+if __name__ == "__main__":
+    unittest.main()
