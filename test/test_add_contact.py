f6f843597d525af6267d459198e45d5ffb67ac8e
Пархимчик Ярослав <yaroslav.parhim2@gmail.com>
2021-07-03 02:28:18 +0300

    без коментариев об этом задании

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,9 +4,10 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contacts_list()
-    app.contact.create(Contact(first_name="sdfdsfg", home_number="fdsfgsdfg", first_mail="sdfgsdffg",
-                               second_address="fgdsfgsdfg"))
+    contact = Contact(first_name="sdfdsfg", home_number="fdsfgsdfg", first_mail="sdfgsdffg",
+            second_address="fgdsfgsdfg", last_name="dsgdsfg")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts[0:1] = []
+    old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
