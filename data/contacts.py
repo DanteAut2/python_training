d0575a74f0b6332dd21060a501bcb2107d228e56
DanteAut2 <79665248+DanteAut2@users.noreply.github.com>
2021-07-21 01:50:27 +0300

    Не работает комит по видео
    Ещё один способ параметризации тестов: динамическая генерация тестов

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+
+constant = [
+    Contact(lastname="name1", firstname="header1", homephone="footer1", first_mail="gdfg", second_address="fdsfds"),
+    Contact(lastname="name2", firstname="header2", homephone="footer2", first_mail="gd1fg", second_address="fds1fds")
+]
