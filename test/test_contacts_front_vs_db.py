25ab38343a4cf45d81713e26254ef766cd05cac9
DanteAut2 <79665248+DanteAut2@users.noreply.github.com>
2021-07-30 01:28:08 +0300

    подсмотрел, но 21 сделал

--- test/test_contacts_front_vs_db.py
+++ test/test_contacts_front_vs_db.py
@@ -3,31 +3,16 @@ from model.contact import Contact
 
 
 def test_all_contact_data_on_home_page(app, db):
+    check_empty_filling_db(app, db)
     contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
     contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
-    assert len(contact_from_home_page) == len(contact_from_db)
-    for i in range(len(contact_from_home_page)):
-        assert contact_from_home_page[i].id == contact_from_db[i].id
-        assert contact_from_home_page[i].firstname == contact_from_db[i].firstname
-        assert contact_from_home_page[i].lastname == contact_from_db[i].lastname
-        assert contact_from_home_page[i].address == contact_from_db[i].address
-        assert contact_from_home_page[i].all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db[i])
-        assert contact_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db[i])
-
+    assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
 
 def clean(s):
     return re.sub("[() -]", "", s)
 
 
-def merge_emails_like_on_home_page(contact_helper):
-     return "\n".join(filter(lambda x: x!= "", map(lambda x: clean(x),
-                                                  filter(lambda x: x is not None, [contact_helper.email,
-                                                                                contact_helper.email2,
-                                                                                contact_helper.email3]))))
-
-
-def merge_phones_like_on_home_page(contact_helper):
-    return "\n".join(filter(lambda x: x!= "", map(lambda x: clean(x),
-                                                  filter(lambda x: x is not None, (contact_helper.homephone,
-                                                         contact_helper.mobilephone, contact_helper.workphone,
-                                                                                   contact_helper.secondaryphone)))))
+def check_empty_filling_db(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="figu", lastname="ihfuge", mobile="74575", homephone="65476", workphone="576", secondaryphone="33",
+                      address='jediafhc', email='odfej', email2='frdj', email3='jdfr'))
