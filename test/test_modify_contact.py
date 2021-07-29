370022c68c87af112fc7773cf7ee6ae8df93c604
DanteAut2 <79665248+DanteAut2@users.noreply.github.com>
2021-07-30 00:39:52 +0300

    Переделал 20 задание, забыл асерты

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -10,6 +10,7 @@ def test_modify_contact(app, db, check_ui):
     new_contact = Contact(firstname="sdfdsfg", first_mail="sdfgsdffg", second_address="fgdsfgsdfg")
     app.contact.modify_contact_by_id(contact.id, new_contact)
     new_contacts = db.get_contact_list()
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                      key=Contact.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,5 +9,7 @@ def test_modify_group(app, db, check_ui):
     new_group = Group(name="sdfgdsfg", footer="gdfhgdfghd")
     app.group.modify_group_by_id(group.id, new_group)
     new_groups = db.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
